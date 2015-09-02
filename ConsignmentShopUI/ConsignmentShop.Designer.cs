9ba3336b80a3d0a7d21e6c3ad11e6c904ef41504
khaliqqq <akhaliq3@live.utm.my>
2015-09-03 02:05:01 +0800

    init

---
+++ ConsignmentShopUI/ConsignmentShop.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ConsignmentShopUI
+{
+    partial class ConsignmentShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerText = new System.Windows.Forms.Label();
+            this.itemsListBox = new System.Windows.Forms.ListBox();
+            this.itemListBoxLabel = new System.Windows.Forms.Label();
+            this.addToCart = new System.Windows.Forms.Button();
+            this.shoppingCartListBoxLabel = new System.Windows.Forms.Label();
+            this.shoppingCartListBox = new System.Windows.Forms.ListBox();
+            this.makePurchase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerText
+            // 
+            this.headerText.AutoSize = true;
+            this.headerText.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerText.Location = new System.Drawing.Point(12, 9);
+            this.headerText.Name = "headerText";
+            this.headerText.Size = new System.Drawing.Size(403, 45);
+            this.headerText.TabIndex = 0;
+            this.headerText.Text = "Consignment Shop Demo";
+            // 
+            // itemsListBox
+            // 
+            this.itemsListBox.FormattingEnabled = true;
+            this.itemsListBox.ItemHeight = 21;
+            this.itemsListBox.Location = new System.Drawing.Point(20, 112);
+            this.itemsListBox.Name = "itemsListBox";
+            this.itemsListBox.Size = new System.Drawing.Size(297, 172);
+            this.itemsListBox.TabIndex = 1;
+            // 
+            // itemListBoxLabel
+            // 
+            this.itemListBoxLabel.AutoSize = true;
+            this.itemListBoxLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemListBoxLabel.Location = new System.Drawing.Point(16, 88);
+            this.itemListBoxLabel.Name = "itemListBoxLabel";
+            this.itemListBoxLabel.Size = new System.Drawing.Size(96, 21);
+            this.itemListBoxLabel.TabIndex = 2;
+            this.itemListBoxLabel.Text = "Store Items";
+            // 
+            // addToCart
+            // 
+            this.addToCart.Location = new System.Drawing.Point(323, 167);
+            this.addToCart.Name = "addToCart";
+            this.addToCart.Size = new System.Drawing.Size(125, 32);
+            this.addToCart.TabIndex = 3;
+            this.addToCart.Text = "Add To Cart ->";
+            this.addToCart.UseVisualStyleBackColor = true;
+            // 
+            // shoppingCartListBoxLabel
+            // 
+            this.shoppingCartListBoxLabel.AutoSize = true;
+            this.shoppingCartListBoxLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shoppingCartListBoxLabel.Location = new System.Drawing.Point(450, 88);
+            this.shoppingCartListBoxLabel.Name = "shoppingCartListBoxLabel";
+            this.shoppingCartListBoxLabel.Size = new System.Drawing.Size(119, 21);
+            this.shoppingCartListBoxLabel.TabIndex = 5;
+            this.shoppingCartListBoxLabel.Text = "Shopping Cart";
+            // 
+            // shoppingCartListBox
+            // 
+            this.shoppingCartListBox.FormattingEnabled = true;
+            this.shoppingCartListBox.ItemHeight = 21;
+            this.shoppingCartListBox.Location = new System.Drawing.Point(454, 112);
+            this.shoppingCartListBox.Name = "shoppingCartListBox";
+            this.shoppingCartListBox.Size = new System.Drawing.Size(297, 172);
+            this.shoppingCartListBox.TabIndex = 4;
+            // 
+            // makePurchase
+            // 
+            this.makePurchase.Location = new System.Drawing.Point(661, 290);
+            this.makePurchase.Name = "makePurchase";
+            this.makePurchase.Size = new System.Drawing.Size(90, 32);
+            this.makePurchase.TabIndex = 6;
+            this.makePurchase.Text = "Purchase";
+            this.makePurchase.UseVisualStyleBackColor = true;
+            // 
+            // ConsignmentShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 519);
+            this.Controls.Add(this.makePurchase);
+            this.Controls.Add(this.shoppingCartListBoxLabel);
+            this.Controls.Add(this.shoppingCartListBox);
+            this.Controls.Add(this.addToCart);
+            this.Controls.Add(this.itemListBoxLabel);
+            this.Controls.Add(this.itemsListBox);
+            this.Controls.Add(this.headerText);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ConsignmentShop";
+            this.Text = "Consignment Shop Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerText;
+        private System.Windows.Forms.ListBox itemsListBox;
+        private System.Windows.Forms.Label itemListBoxLabel;
+        private System.Windows.Forms.Button addToCart;
+        private System.Windows.Forms.Label shoppingCartListBoxLabel;
+        private System.Windows.Forms.ListBox shoppingCartListBox;
+        private System.Windows.Forms.Button makePurchase;
+    }
+}
+
---
+++ ConsignmentShopUI/ConsignmentShop.cs
@@ -0,0 +1,72 @@
+﻿using ConsignmentShopLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsignmentShopUI
+{
+    public partial class ConsignmentShop : Form
+    {
+        private Store store = new Store();
+        private List<Item> shoppingCartData = new List<Item>();
+        BindingSource itemsBinding = new BindingSource();
+
+
+        public ConsignmentShop()
+        {
+            InitializeComponent();
+            SetupData();
+
+            itemsBinding.DataSource = store.Items;
+            itemsListBox.DataSource = itemsBinding;
+
+            itemsListBox.DisplayMember = "Display";
+            itemsListBox.ValueMember = "Display";
+        }
+
+        private void SetupData()
+        {
+            store.Vendors.Add(new Vendor { FirstName = "Smith", LastName = "Bill" });
+            store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Jones" });
+
+            store.Items.Add(new Item
+            {
+                Title = "Naruto Shippudden",
+                Description = "A tale about Ninja",
+                Price = 4.50M, Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Fairy Tail",
+                Description = "The advanture of Fairy Tail's wizards",
+                Price = 2.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Dragon Ball Z",
+                Description = "Goku and friends adventure",
+                Price = 14.50M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Bleach",
+                Description = "Ichigo Kurosaki and friends",
+                Price = 42.50M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Name = "Seconds are better";
+        }
+    }
+}
